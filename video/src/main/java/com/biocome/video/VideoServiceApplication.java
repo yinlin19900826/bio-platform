e2676676c74ffdbe29d40de333966755907f4c17
lenovo <yinlin19900826@126.com>
2019-07-10 17:10:36 +0800

    video基础搭建

---
+++ video/src/main/java/com/biocome/video/VideoServiceApplication.java
@@ -0,0 +1,15 @@
+package com.biocome.video;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class VideoServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VideoServiceApplication.class, args);
+    }
+}
