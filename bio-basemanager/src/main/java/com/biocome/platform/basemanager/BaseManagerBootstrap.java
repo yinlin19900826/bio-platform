8ecab12c4fb42606fe7c02cb253fd3367d1a91dc
hanxiangyu <hanxiangyu@biocome.com>
2019-07-12 14:21:35 +0800

    提交基础管理服务

---
+++ bio-basemanager/src/main/java/com/biocome/platform/basemanager/BaseManagerBootstrap.java
@@ -0,0 +1,33 @@
+package com.biocome.platform.basemanager;
+
+import com.ace.cache.EnableAceCache;
+import com.biocome.platform.auth.client.EnableAceAuthClient;
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author hxy
+ * @date 2019/7/12 13:51
+ */
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@SpringBootApplication
+@EnableFeignClients({"com.biocome.platform.auth.client.feign", "com.biocome.platform.basemanager.rpc.service"})
+@EnableScheduling
+@EnableAceAuthClient
+@EnableAceCache
+@EnableTransactionManagement
+@MapperScan("com.biocome.platform.basemanager.mapper")
+@EnableSwagger2Doc
+public class BaseManagerBootstrap {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(BaseManagerBootstrap.class).run(args);
+    }
+}
