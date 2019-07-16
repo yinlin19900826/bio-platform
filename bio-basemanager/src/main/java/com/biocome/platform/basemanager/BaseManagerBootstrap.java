bb3afff2e0fc1d8776b07e2964728db5fd3b9bd2
hanxiangyu <hanxiangyu@biocome.com>
2019-07-16 17:14:55 +0800

    扫描包

--- bio-basemanager/src/main/java/com/biocome/platform/basemanager/BaseManagerBootstrap.java
+++ bio-basemanager/src/main/java/com/biocome/platform/basemanager/BaseManagerBootstrap.java
@@ -9,6 +9,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -26,6 +27,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @MapperScan({"com.biocome.platform.basemanager.mapper", "com.biocome.platform.inter.basemanager.mapper"})
 @EnableSwagger2Doc
+@ComponentScan({"com.biocome.platform"})
 public class BaseManagerBootstrap {
     public static void main(String[] args) {
         new SpringApplicationBuilder(BaseManagerBootstrap.class).run(args);
