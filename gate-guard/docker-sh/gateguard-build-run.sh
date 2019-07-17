87e0339672f09482e1c8b19a96e83cbd4488fc2a
zengqiang <zengqiang@biocome.com>
2019-07-17 18:29:37 +0800

    docker-sh提交

---
+++ gate-guard/docker-sh/gateguard-build-run.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+REPOSITORIES='gate-gurad'
+DATE=`date +%Y%m%d%H%M%S`
+
+mv /home/biocome/gate-guard/gate-guard.jar /home/biocome/backups/gate-guard-${DATE}.jar
+
+rm -rf /home/biocome/gate-guard/gate-guard.jar
+cp /home/jenkins/workspace/Automatic-deployment/gate-guard/target/gate-guard.jar /home/biocome/gate-guard/
+
+# Stop container, and delete the container.
+CONTAINER_ID=`docker ps | grep ${REPOSITORIES} | awk '{print $1}'`
+if [ -n "$CONTAINER_ID" ]; then
+    docker stop $CONTAINER_ID
+    docker rm $CONTAINER_ID
+else #如果容器启动时失败了，就需要docker ps -a才能找到那个容器
+    CONTAINER_ID=`docker ps -a | grep ${REPOSITORIES} | awk '{print $1}'`
+    if [ -n "$CONTAINER_ID" ]; then  # 如果是第一次在这台机器上拉取运行容器，那么docker ps -a也是找不到这个容器的
+        docker rm $CONTAINER_ID
+    fi
+fi
+# Delete image early version.
+IMAGE_ID=`sudo docker images | grep ${REPOSITORIES} | awk '{print $3}'`
+if [ -n "${IMAGE_ID}" ];then
+    docker rmi ${IMAGE_ID}
+fi
+
+rm -rf /home/biocome/gate-guard/Dockerfile
+
+cat >>/home/biocome/gate-guard/Dockerfile<<EOF
+FROM livingobjects/jre8
+VOLUME /tmp
+ADD gate-guard.jar app.jar
+RUN bash -c 'touch /app.jar'
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+EOF
+
+cd /home/biocome/gate-guard
+TAG=`date +%Y%m%d`
+PRE='bio'
+# Build
+docker build -t ${PRE}/${REPOSITORIES}:${TAG} . &>/dev/null
+
+# Run.
+docker run -d --name ${REPOSITORIES} -p 8777:8777 ${PRE}/${REPOSITORIES}:${TAG}
