3db82b89150befbfc44e001b3cb91e17449cdbb7
hanxiangyu <hanxiangyu@biocome.com>
2019-07-29 14:00:36 +0800

    小程序项目

---
+++ wechatapplet/src/main/java/com/biocome/platform/wechatApplet/WetCartAppletBootstrap.java
@@ -0,0 +1,33 @@
+package com.biocome.platform.wechatApplet;
+
+import com.biocome.platform.auth.client.EnableAceAuthClient;
+import com.spring4all.swagger.EnableSwagger2Doc;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author hxy
+ * @date 2019/7/29 11:35
+ */
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@SpringBootApplication
+@EnableFeignClients({"com.biocome.platform.auth.client.feign", "com.biocome.platform.wechatApplet.rpc.service"})
+@EnableScheduling
+@EnableAceAuthClient
+@EnableTransactionManagement
+@MapperScan({"com.biocome.platform.wechatApplet.mapper", "com.biocome.platform.inter.basemanager.mapper"})
+@EnableSwagger2Doc
+@ComponentScan({"com.biocome.platform"})
+public class WetCartAppletBootstrap {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(WetCartAppletBootstrap.class).run(args);
+    }
+}
