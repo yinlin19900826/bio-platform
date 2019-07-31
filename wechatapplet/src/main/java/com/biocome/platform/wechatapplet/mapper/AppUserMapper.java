ccb8514bcdeb94c3b7a339d58029e3c3f38f2375
zengqiang <zengqiang@biocome.com>
2019-07-31 18:35:07 +0800

    1.添加app user

---
+++ wechatapplet/src/main/java/com/biocome/platform/wechatapplet/mapper/AppUserMapper.java
@@ -0,0 +1,15 @@
+package com.biocome.platform.wechatapplet.mapper;
+
+import com.biocome.platform.wechatapplet.entity.AppUser;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * 
+ * 
+ * @author zengqiang
+ * @email zengqiang724@163.com
+ * @date 2019-07-31 18:22:58
+ */
+public interface AppUserMapper extends Mapper<AppUser>{
+	
+}
