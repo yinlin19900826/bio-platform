7b3c652cd15c6a3b411fc0edc1b70108eaed55bb
zengqiang <zengqiang@biocome.com>
2019-08-12 14:29:53 +0800

    小程序登录返回内容修改

--- auth/auth-server/src/main/java/com/biocome/platform/auth/service/AuthService.java
+++ auth/auth-server/src/main/java/com/biocome/platform/auth/service/AuthService.java
@@ -2,6 +2,7 @@ package com.biocome.platform.auth.service;
 
 
 import com.biocome.platform.auth.util.user.JwtAuthenticationRequest;
+import com.biocome.platform.common.msg.ObjectRestResponse;
 
 import java.util.Map;
 
@@ -9,5 +10,5 @@ public interface AuthService {
     String login(JwtAuthenticationRequest authenticationRequest) throws Exception;
     String refresh(String oldToken) throws Exception;
     void validate(String token) throws Exception;
-    String appLogin(JwtAuthenticationRequest authenticationRequest) throws Exception;
+    ObjectRestResponse<String> appLogin(JwtAuthenticationRequest authenticationRequest) throws Exception;
 }
